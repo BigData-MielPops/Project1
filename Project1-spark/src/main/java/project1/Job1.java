8277e2078b922eabd42c9878bbab12cdf39b4657
Veronica Iovinella <vero.neeja@gmail.com>
2017-05-05 23:38:00 +0200

    added first job with spark

---
+++ Project1-spark/src/main/java/project1/Job1.java
@@ -0,0 +1,90 @@
+package project1;
+
+import org.apache.spark.api.java.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.apache.spark.SparkConf;
+
+import scala.Tuple2;
+
+
+public final class Job1 {
+
+	private static String separator = "\t";
+	private static JavaSparkContext ctx;
+
+	public static void main(String[] args) throws Exception {
+		if (args.length < 2) {
+			System.err.println("Usage: Job1 <input file> <output file>");
+			System.exit(1);
+		}
+		SparkConf sparkConf = new SparkConf().setAppName("Job1");
+		ctx = new JavaSparkContext(sparkConf);
+		
+		
+		JavaRDD<String> csvLines = ctx.textFile(args[0], 1);
+		JavaRDD<List<String>> lines = csvLines.map(s -> new ArrayList<String>(Arrays.asList(s.split(separator))));
+
+		JavaPairRDD<String, Tuple2<Integer, Integer>> countByMonthProduct =
+				lines.mapToPair(line -> 
+					new Tuple2<>(
+							timestampToMonth(Long.parseLong(line.get(7)) * 1000) + separator + line.get(1), 
+							new Tuple2<>(Integer.parseInt(line.get(6)), 1)
+						)
+					).reduceByKey((a, b) -> 
+					new Tuple2<>(
+							a._1() + b._1(), // total score by month+product
+							a._2() + b._2() // count by month+product
+						)
+					);
+		
+		JavaPairRDD<String, Iterable<String>> averageAndProductListByMonth = 
+				countByMonthProduct.mapToPair(reducedLine -> 
+					new Tuple2<>(
+						reducedLine._1().split(separator)[0], // month
+						String.valueOf(
+								(float)reducedLine._2()._1() / reducedLine._2()._2()) + separator + reducedLine._1().split(separator)[1]
+							) // (product \t average)
+					).groupByKey(1);
+		
+		JavaPairRDD<String, List<String>> topFiveProductsByMonth = 
+				averageAndProductListByMonth.mapToPair(reGroupedLine ->
+					new Tuple2<>(
+							reGroupedLine._1(),
+								Arrays.asList((takeFirst(reGroupedLine._2(), 5))).stream().filter(Objects::nonNull).collect(Collectors.toList())
+							)
+				);
+		
+		topFiveProductsByMonth.coalesce(1).sortByKey(true).saveAsTextFile(args[1]);
+		
+		ctx.stop();
+	}
+	
+	
+	public static String timestampToMonth(Long timestamp) {
+		Date date = new Date(timestamp);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		Integer year = cal.get(Calendar.YEAR);
+		Integer month = cal.get(Calendar.MONTH)+1;
+		
+		return String.valueOf(year) + String.format("%02d", month);
+	}
+	
+	public static String[] takeFirst(Iterable<String> iterable, Integer howMany) {
+		String[] app = new String[howMany];
+		
+		for (String data : iterable) {
+			Util.insertInOrderByScore(data, app);
+		}
+		
+		return app;
+	}
+}
---
+++ Project1-spark/src/main/java/project1/Util.java
@@ -0,0 +1,43 @@
+package project1;
+
+public class Util {
+
+	private static String separator = "\t";
+
+	public static void insertInOrderByScore(String val, String[] floats){
+		System.out.println("==insert==");
+		System.out.println(val);
+		int i;
+		for(i=0; i<floats.length; i++){
+			try {
+				System.out.print(floats[i] + " ");
+				System.out.println();
+				if(Float.parseFloat(floats[i].split(separator)[0]) < Float.parseFloat(val.split(separator)[0]))
+					break;
+			}
+			catch (Exception e) {
+				break;
+			}
+		}
+		System.out.println(i);
+		if(i<floats.length){
+			for(int k=floats.length-2; k>=i; k--){
+				System.out.print(floats[k] + " ");
+				floats[k+1]=floats[k];            
+			}
+			System.out.println();
+
+			floats[i]=val;
+		}
+	}
+
+	public static String orderCouple(String s1, String s2) {
+		return orderCouple(s1, s2, separator);
+	}
+
+	public static String orderCouple(String s1, String s2, String separator) {
+		if(s1.compareTo(s2) < 0) return s1 + separator + s2;
+		//else
+		return s2 + separator + s1;
+	}
+}
